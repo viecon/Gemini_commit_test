3bd7189c2eab190b04cf78a10413b5ae2aadf697
shawn28e7 <twshawn28e7@gmail.com>
2025-04-25 20:09:56 +0800

    remove obj_dir

--- hw01/obj_dir/Vhw1_tb__ALL.d
+++
@@ -1,7 +0,0 @@
-Vhw1_tb__ALL.o: Vhw1_tb__ALL.cpp Vhw1_tb.cpp Vhw1_tb.h \
- /usr/share/verilator/include/verilated_heavy.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h Vhw1_tb__Syms.h \
- Vhw1_tb__Trace.cpp /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated_trace.h Vhw1_tb__Slow.cpp \
- Vhw1_tb__Syms.cpp Vhw1_tb__Trace__Slow.cpp
--- hw01/obj_dir/hw1_tb.d
+++
@@ -1,6 +0,0 @@
-hw1_tb.o: ../hw1_tb.cpp /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_trace.h Vhw1_tb.h \
- /usr/share/verilator/include/verilated_heavy.h
