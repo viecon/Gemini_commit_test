7fd36377cddda0b09228843b555577edfa859675
shawn28e7 <twshawn28e7@gmail.com>
2025-04-25 20:16:50 +0800

    update hw01 to verilator 5.x

--- hw01/hw1_tb.v
+++ hw01/hw1_tb.v
@@ -1,22 +1,32 @@
-module hw1_tb(input clk);
-reg [15:0] A;
-reg [7:0] x;
-wire [17:0] y;
-reg [17:0] y_true;
-matrix_top UUT(.A(A), .x(x), .y(y));
+module hw1_tb;
+    reg clk = 0;
+    reg [15:0] A;
+    reg [7:0] x, tick_count = 0;
+    wire [17:0] y;
+    reg [17:0] y_true;
+    matrix_top UUT(.A(A), .x(x), .y(y));
+    initial begin
+        $dumpfile("waveform.vcd");
+        $dumpvars(0, hw1_tb);
+    end
 
-always @(posedge clk) begin
-    A <= { 4'd15, 4'd7, 4'd3, 4'd10 };
-    x <= { 4'd6, 4'd11 };
-    y_true <= { 9'd167, 9'd128 };
-end
+    always begin
+        #5 clk = !clk;
+    end
 
-always @(negedge clk) begin
-    if (y != y_true)
-        $display("incorrect!");
-    else
-        $display("correct");
-    $finish;
-end
+    always @(posedge clk) begin
+        A <= { 4'd15, 4'd7, 4'd3, 4'd10 };
+        x <= { 4'd6, 4'd11 };
+        y_true <= { 9'd167, 9'd128 };
+        tick_count <= tick_count + 1;
+    end
 
+
+    always @(posedge clk) begin
+        if (tick_count == 8'd1) begin
+            if (y != y_true) $display("incorrect!");
+            else $display("correct");
+            $finish;
+        end
+    end
 endmodule
--- hw01/test.sh
+++ hw01/test.sh
@@ -1,4 +1,3 @@
-verilator -trace -cc hw1_tb.v -exe hw1_tb.cpp 
-make -C obj_dir -f Vhw1_tb.mk Vhw1_tb
+verilator --trace --binary -j 0 -Wall hw1_tb.v
 ./obj_dir/Vhw1_tb
-gtkwave waveform.vcd
+# gtkwave waveform.vcd
